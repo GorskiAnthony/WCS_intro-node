a74753c71ae03d5057083b1d0dec4e853b4da49b
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-03-17 12:23:26 +0100

    intro nodejs

---
+++ index.js
@@ -0,0 +1,13 @@
+const chalk = require("chalk");
+
+// const multiply = require("./multiply.js");
+const multiply = require("./multiply");
+
+// const c = 5;
+// const d = 8;
+
+// console.log(multiply(5, d));
+
+console.log(chalk.blue("Hello World!"));
+console.log(chalk.bgGreen("Hello World!"));
+console.log(chalk.yellow("Hello World!"));
